d1ce6a55cee3a6b8e28e0c61bb3926a4895af3a2
Constandinos Demetriou <kdemet06@cs.ucy.ac.cy>
2021-03-02 01:24:25 +0200

    fixed bugs about edges creation

--- process_dataset.py
+++ process_dataset.py
@@ -2,17 +2,26 @@ import pandas as pd
 from tqdm import tqdm
 
 # label columns
+
 mainFields = ['id', 'uploader', 'age', 'category', 'length', 'views', 'rate', 'ratings', 'comments']
 relatedVideos = ['relatedID_1', 'relatedID_2', 'relatedID_3', 'relatedID_4', 'relatedID_5', 'relatedID_6',
                  'relatedID_7', 'relatedID_8', 'relatedID_9', 'relatedID_10', 'relatedID_11', 'relatedID_12',
                  'relatedID_13', 'relatedID_14', 'relatedID_15', 'relatedID_16', 'relatedID_17', 'relatedID_18',
                  'relatedID_19', 'relatedID_20']
+# datasets directories
+datasets = ['dataset/0.txt', 'dataset/1.txt', 'dataset/2.txt', 'dataset/3.txt']
 
 
-def read_data():
+def read_data(filenames):
     """
     This function will read the dataset from files into a dataframe.
-    Also, this function will drop rows with empty values.
+    Also, this function will drop rows with empty values. Moreover,
+    this function will write dataset in a file.
+
+    Parameters
+    ----------
+    filenames: list
+        A list with the datasets directories
 
     Returns
     -------
@@ -21,13 +30,11 @@ def read_data():
     """
 
     # read dataset into dataframes
-    df_0 = pd.read_csv('dataset/0.txt', sep='\t', header=None)
-    df_1 = pd.read_csv('dataset/1.txt', sep='\t', header=None)
-    df_2 = pd.read_csv('dataset/2.txt', sep='\t', header=None)
-    df_3 = pd.read_csv('dataset/3.txt', sep='\t', header=None)
-
-    # concat dataframes into a single dataframe
-    df = pd.concat([df_0, df_1, df_2, df_3])
+    df = pd.DataFrame()
+    for file in filenames:
+        df_temp = pd.read_csv(file, sep='\t', header=None)
+        # concat dataframes into a single dataframe
+        df = pd.concat([df, df_temp])
 
     # add a label in each column
     df.columns = mainFields + relatedVideos
@@ -38,6 +45,9 @@ def read_data():
     # reset index
     df.reset_index(inplace=True)
     df = df.drop(columns=['index'])
+
+    # write dataset a file
+    df.to_csv('dataset.csv', index=False)
 
     return df
 
@@ -53,94 +63,88 @@ def rename_node(df):
 
     Returns
     -------
-    nodes_id: list
-        A list with nodes id
-    renaming: dict
-        The corresponding between node id and a integer number
+    df: numpy array
+        The dataset with rename id
     """
 
-    # create a list with unique nodes id
-    nodes_id = list(set(df['id'].values))
+    # add a new column with a unique integer number to each node
+    rename_id = list(range(0, len(df)))
+    df['rename_id'] = rename_id
 
-    # create an empty Dictionary
-    renaming = {}
+    # reorder columns in dataframe
+    columns = []
+    columns.append('rename_id')
+    columns += mainFields
+    columns += relatedVideos
+    df = df[columns]
 
-    # give a unique integer number to each node
-    for i in range(0, len(nodes_id)):
-        renaming[nodes_id[i]] = i
-
-    return nodes_id, renaming
+    return df
 
 
-def create_nodes(df, nodes_id, renaming):
+def create_nodes(df):
     """
-    This function will give a unique integer number to each node id.
+    This function will write in a file the informations for each node.
 
     Parameters
     ----------
     df: numpy array
         The dataset
-    nodes_id: list
-        A list with nodes id
-    renaming: dict
-        The corresponding between node id and a integer number
     """
 
-    # copy some columns to another dataframe
-    nodes_df = df[['id', 'uploader', 'age', 'category', 'length', 'views', 'rate', 'ratings', 'comments']].copy()
-
-    # replace node id with integer number
-    for i in tqdm(range(0, len(nodes_df['id']))):
-        nodes_df.loc[nodes_df['id'][i]] = renaming[nodes_df['id'][i]]
+    # get some columns to another dataframe
+    nodes_df = df[['rename_id', 'id', 'uploader', 'age', 'category', 'length', 'views', 'rate', 'ratings', 'comments']]
 
     # write nodes in a file
-    nodes_df.to_csv('nodes.csv')
+    nodes_df.to_csv('nodes.csv', index=False)
 
 
-def create_edges(df, nodes_id, renaming):
+def create_edges(df):
     """
-    This function will give a unique integer number to each node id.
+    This function will create the edges between nodes and will write them into a file.
 
     Parameters
     ----------
     df: numpy array
         The dataset
-    nodes_id: list
-        A list with nodes id
-    renaming: dict
-        The corresponding between node id and a integer number
     """
+
+    # create a list with nodes id
+    nodes_id = list(df['id'])
 
     # capture edges in 2 separate lists
     node_list_1 = []
     node_list_2 = []
-    for index, row in tqdm(df.iterrows()):
-        n1_id = row['id']
+
+    # find edges
+    for row in tqdm(df.index):
+        n1_rename_id = df['rename_id'][row]
         for related in relatedVideos:
-            n2_id = row[related]
+            n2_id = df[related][row]
             if n2_id in nodes_id:
-                node_list_1.append(renaming[n1_id])
-                node_list_2.append(renaming[n2_id])
+                n2_rename_id = df[df['id'] == n2_id]['rename_id'].values[0]
+                node_list_1.append(n1_rename_id)
+                node_list_2.append(n2_rename_id)
 
     # insert edges in a dataframe
     edges_df = pd.DataFrame({'node_1': node_list_1, 'node_2': node_list_2})
 
     # write edges a file
-    edges_df.to_csv('edges.csv')
+    edges_df.to_csv('edges.csv', index=False)
 
 # =============================================================================#
 #                                      MAIN                                    #
 # =============================================================================#
 
+
 # read dataset
-df = read_data()
+df = read_data(datasets)
 
 # rename nodes id
-nodes_id, renaming = rename_node(df)
+df = rename_node(df)
 
 # create nodes
-create_nodes(df, nodes_id, renaming)
+create_nodes(df)
 
 # create edges
-create_edges(df, nodes_id, renaming)
+create_edges(df)
 
