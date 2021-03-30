39e08ce394a152e78acc2acdc5adbfc4959cd0f4
Constandinos Demetriou <kdemet06@cs.ucy.ac.cy>
2021-03-31 01:23:10 +0300

    update run script

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ for val in ${StringArray[@]}; do
 	
 	python process_dataset.py $val".txt" > ../results/$val".out" 2> ../results/$val".err"
 	python featurization.py $val"_edges.csv" >> ../results/$val".out" 2>> ../results/$val".err"
-	python models.py $val"_edges_features.csv" >> ../results/$val".out" 2>> ../results/$val".err"
+	python models.py $val"_edges_features.csv" 7 >> ../results/$val".out" 2>> ../results/$val".err"
 	
 	cd ..
 	mv "results" "results_"$val
